2e345128a94286af280c8de6476b1eeda807c785
AndryFMX <afmandry@gmail.com>
2026-07-10 12:51:21 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,13 @@ TOKEN="8926173750:AAEl_wflDiJPtY6aDQwbOG-yOqqZyfHhBos"
 CHAT_ID="@lauramendosaf777"
 
 TEXT="""
-Hola 👋
+AMOR AQUÍ EL RETO 🚫‼️🔴
+🤤🌶️🔥🙈👇🏻
+https://miraelretoenvivo.short.gy/laura
 
-Este mensaje fue enviado automáticamente.
+SI NO LES FUNCIONA EL LINK ENTREN AQUÍ
+👇🏻🙈🔥💕
+https://miraelretoenvivo.short.gy/laura
 """
 
 url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
